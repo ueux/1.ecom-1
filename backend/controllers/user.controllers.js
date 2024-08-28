155942f2a5597958da957818b856913fed45dad7
Harsh Kumar <hemenderkumar3000@gmail.com>
2024-08-28 21:14:51 +0530

    login logout and  authentication added

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -22,9 +22,6 @@ const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (
     [ email, username, password].some((field) => field?.trim() === "")
-    // !username ||
-    // !email ||
-    // !password
   ) {
     throw new ApiError(400, "All fields are required");
   }
@@ -62,4 +59,77 @@ const createUser = asyncHandler(async (req, res) => {
 
     }, "User registered Successfully"));
 });
-export { createUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+  if (!username && !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+  if (!password) {
+    throw new ApiError(400, "pasword is required");
+  }
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+  const { accessToken} = await generateAccessToken(
+    user._id
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password "
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1, // this removes the field from document
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    expires:new Date(0),
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"));
+});
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  return res
+    .status(200)
+    .json(new ApiResponse(200, users, "All Users"));
+})
+
+export { createUser,loginUser ,logoutUser ,getAllUsers};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controllers.js";
+import { createUser, loginUser, logoutUser } from "../controllers/user.controllers.js";
+
+import { onlyForAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
 router.route('/signin').post(createUser)
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT,logoutUser);
 
 export default router;
