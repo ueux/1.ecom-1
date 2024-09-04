50c68ec06b9eacff33fb6cf09141a21a5d4dfa38
Harsh Kumar <hemenderkumar3000@gmail.com>
2024-09-04 07:56:33 +0530

    image issue solved

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/products", productRoutes)
 app.use("/api/v1/upload", uploadRoutes)
 app.use("/api/v1/orders", orderRoutes)
+app.use('/uploads', express.static('uploads'));
 app.get("/api/config/paypal", (req, res) => {
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
   });
